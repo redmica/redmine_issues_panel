2e79df6ce6c5dc71d19d28b37feb1eeb42886950
Takenori Takaki <takenory@gmail.com>
2024-11-15 14:24:11 +0900

    Support for SVG icons in Redmine6 (#61)

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_issues_panel do
   name 'Redmine Issues Panel plugin'
   author 'Takenori Takaki (Far End Technologies)'
   description 'A plugin for Redmine to display issues by statuses and change it\'s status by DnD.'
-  requires_redmine version_or_higher: '4.1'
+  requires_redmine version_or_higher: '6.0'
   version '1.0.4'
   url 'https://github.com/redmica/redmine_issues_panel'
   author_url 'https://hosting.redmine.jp/'
